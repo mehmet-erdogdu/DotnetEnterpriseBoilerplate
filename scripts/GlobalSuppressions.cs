a4b56b1c106a8079a2d7715fb7aac0ad822f3c52
Mehmet Erdoğdu <mehmet.erdogdu52@hotmail.com>
2025-09-13 09:52:02 +0300

    test

---
+++ scripts/GlobalSuppressions.cs
@@ -0,0 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: ExcludeFromCodeCoverage]
+[assembly: SuppressMessage("Design", "CA1050:Declare types in namespaces", Justification = "Scripts don't need to be in a namespace")]
